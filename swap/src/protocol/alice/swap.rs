65efc9b4c19489df1b2b5c68ae85007a26d80008
pokkst <pokkst@protonmail.com>
2023-10-11 20:19:38 -0500

    Try 0-conf for Alice again to send lock proof faster (Bob still waits for 10 confs)

--- swap/src/protocol/alice/swap.rs
+++ swap/src/protocol/alice/swap.rs
@@ -139,7 +139,7 @@ where
         } => match state3.expired_timelocks(bitcoin_wallet).await? {
             ExpiredTimelocks::None => {
                 monero_wallet
-                    .watch_for_transfer(state3.lock_xmr_watch_request(transfer_proof.clone(), 1))
+                    .watch_for_transfer(state3.lock_xmr_watch_request(transfer_proof.clone(), 0))
                     .await
                     .with_context(|| {
                         format!(

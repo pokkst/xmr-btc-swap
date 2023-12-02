4b9884bc82e3225506c881bd182c3a24607f23a8
pokkst <pokkst@tutanota.de>
2023-12-02 10:29:19 -0600

    Add multiaddr var to AsbData

--- swap/src/asb/asb_data.rs
+++ swap/src/asb/asb_data.rs
@@ -5,6 +5,7 @@ use serde::Serialize;
 #[serde(rename_all = "camelCase")]
 pub struct AsbData {
     pub peer_id: String,
+    pub multiaddr: String,
     pub balance: u64,
     pub unlocked_balance: u64,
     pub address: String,
